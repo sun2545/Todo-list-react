f851142f4e8b2f4975005aaa46d898fc3a0be97e
sun2545 <kwonsunhye0206@gmail.com>
2018-10-30 23:40:17 -0400

    DUCK

---
+++ src/modules/todos.js
@@ -0,0 +1,43 @@
+import {Map,List} from 'immutable';
+import {handleActions, createAction, handleAction} from 'redux-actions';
+
+const INSERT = 'todos/INSERT';
+const TOGGLE = 'todos/TOGGLE';
+const REMOVE = 'todos/REMOVE';
+
+export const insert =createAction(INSERT);
+export const toggle =createAction(TOGGLE);
+export const remove =createAction(REMOVE);
+
+const initialState =List([
+    Map({
+        id:0,
+        text:'study react',
+        done:true
+    }),
+    map({
+        id:1,
+        text:'styling component',
+        done:false
+    })
+]);
+
+export default handleAction({
+    [INSERT]:(state,action)=>{
+        const {id,text,done}=action.payload;
+return state.push(Map({
+    id,
+    text,
+    done
+}));
+    },
+    [TOGGLE]:(state,action)=>{
+        const{ payload:index}=action;
+        return state.updateIn([index, 'done'],done=>!done);
+    },
+    [REMOVE]: (state,action)=>{
+        const {payload:index}=action;
+        return state.delete(index);
+    }
+
+},initialState)
