2a89e5b6d5380dd9447e33164fbc852674e9ab28
sun2545 <kwonsunhye0206@gmail.com>
2018-10-30 23:25:55 -0400

    create reducers

--- src/modules/input.js
+++ src/modules/input.js
@@ -1,2 +1,16 @@
 import {Map} from 'immutable';
-import {handleActions, createAction} from 'redux-actions';
+import {handleActions, createAction, handleAction} from 'redux-actions';
+
+const SET_INPUT ='input/SET_INPUT';
+
+export const setInput = createAction(SET_INPUT);
+
+const initialState =({
+    value:''
+});
+
+export default handleAction({
+    [SET_INPUT]:(state,action)=>{
+        return state.set('value',action.payload)
+    }
+},initialState);
